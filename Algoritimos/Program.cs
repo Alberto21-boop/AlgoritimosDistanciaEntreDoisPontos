dad40dcf98992d5f3897e701b0f564a7d50adb3a
alberto <albertobarbosa0003@gmail.com>
2022-05-12 17:02:22 -0300

    Adicionar arquivos de projeto.

---
+++ Algoritimos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Algoritimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, y1, y2;
+
+            x1 = 12d;
+            x2 = 13d;
+            y1 = 11d;
+            y2 = 10d;
+            var distancia = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine("A distancia entre os pontos é " + distancia);
+        }
+    }
+}
